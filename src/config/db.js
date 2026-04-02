2d8f78c54aa6ee98b84138a866e05cbec9cf859b
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-04-02 13:52:42 -0400

    fix: env

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 
-    const mongoUri = process.env.MONGO_URI;
+    const mongoUri = process.env.MONGO_URI ?? "mongodb+srv://renancavalcanti_db_user:mcMhLcYMVzaQZObD@cluster0.msfqe8t.mongodb.net/?appName=Cluster0/tt4-2920";
 
     await mongoose.connect(mongoUri);
 
