56c3446a36ed39564d67cff9a79d3c7531daf42d
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-04-09 13:52:39 -0400

    force error node

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const { initializeSocket } = require("./socket");
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
+const startSer
     try{
         await connectDB();
 
